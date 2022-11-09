a027f7f0b9bf8e8799b1aa7804ba04b9f2e75f69
Hiroji <joegilbertdev@outlook.com>
2022-11-09 22:26:47 +0000

    day 2-variables

--- Program.cs
+++ Program.cs
@@ -2,4 +2,63 @@
 
 //create programme dotnet new console -o MyApp -f net7.0
 // run dotnet run
-Console.WriteLine("Program runs");
+
+using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program runs");
+            Console.WriteLine("This is the first line.");
+
+            Console.Write("This is ");
+            Console.Write("the second ");
+            Console.Write("line.");
+        }
+    }
+}
+/*
+//variables
+//int number = 1;
+//const float Pi = 3.14f;
+
+// camelCase -> local var
+//PascalCase -> pascal case
+
+//basic C# types
+int myNum = 5;               // Integer (whole number)
+double myDoubleNum = 5.99D;  // Floating point number
+char myLetter = 'D';         // Character
+bool myBool = true;          // Boolean
+string myText = "Hello";     // String
+
+// non primitive
+string
+array
+enum
+class
+
+
+//can use var keyword to let C# compiler interpret datatype from var
+Overflowing
+
+byte number = 255;
+soppose
+number = number + 1; -> 0 as too large
+
+prevent with check keword
+checked {
+    var declarations
+    aithmatic operations
+}
+
+Scope
+
+
+*/
+
+
+
