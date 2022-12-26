b2bba1e0a2d2b1aec92e8174faa72b23d5da04ad
Hiroji <joegilbertdev@outlook.com>
2022-12-26 21:27:53 +0000

    constructors- this key word added

--- Constructors.cs
+++ Constructors.cs
@@ -1,9 +1,46 @@
-class ConstructorMethods {
-    class Sergal {
+class ConstructorMethods
+{
+
+    //define a new class
+    class Sergal
+    {
+
+        public string name;
+        public int age;
+        // Create a class constructor with multiple parameters
+        public Sergal(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        //methods
+        public void introduceYourself()
+        {
+            Console.WriteLine($"You dare summon me? I am {this.name}.");
+        }
+
 
     }
 
-    public static void runConstructors() {
+
+    //inheritance from Parent
+   
+
+
+    public static void runConstructors()
+    {
         Console.WriteLine("\nRunning program Constructors:");
+
+        Sergal hiroji = new Sergal("Hiroji", 25);
+        Console.WriteLine(hiroji.age);
+        //edit property
+        hiroji.age = 15;
+        Console.WriteLine(hiroji.age);
+
+        //call method
+        hiroji.introduceYourself();
+
+       // Shigu Rain = new Shigu("Rain Silves", 30);
     }
 }
