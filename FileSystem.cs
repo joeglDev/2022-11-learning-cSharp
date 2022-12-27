c81f54921d6fa7a483f5d410c13e96fa80f2b8b9
Hiroji <joegilbertdev@outlook.com>
2022-12-27 20:01:30 +0000

    FileSystem details

--- FileSystem.cs
+++ FileSystem.cs
@@ -1,5 +1,26 @@
-class FileSystemMethods {
-    public static void runFileSystem() {
-        Console.WriteLine("\nRunning program File System:");
+using System.IO;
+
+class FileSystemMethods
+{
+    public static void runFileSystem()
+    {
+        string path = @"./storage.txt";
+
+        // This text is added only once to the file.
+        if (!File.Exists(path))
+        {
+            // Create a file to write to.
+            string createText = "Hello and Welcome" + Environment.NewLine;
+            File.WriteAllText(path, createText);
+        }
+
+        // This text is always added, making the file longer over time
+        // if it is not deleted.
+        string appendText = "This is extra text" + Environment.NewLine;
+        File.AppendAllText(path, appendText);
+
+        // Open the file to read from.
+        string readText = File.ReadAllText(path);
+        Console.WriteLine(readText);
     }
 }
